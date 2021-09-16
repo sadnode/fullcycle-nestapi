bb25939b35d5e8e7a77dc47ace8b11be8d0cd681
Igor <igor_csns_s2@hotmail.com>
2021-09-16 15:33:29 -0300

    feat(api): active cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
       errorHttpStatusCode: 422,
     }),
   );
-
 
   await app.listen(3000);
 }
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,17 +1,13 @@
-import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
 import { TenantGuard } from 'src/tenant/tenant.guard';
-import { TenantService } from 'src/tenant/tenant/tenant.service';
 
 @UseGuards(JwtAuthGuard, TenantGuard)
 @Controller('transactions')
 export class TransactionsController {
-  constructor(
-    private readonly transactionsService: TransactionsService,
-    private tenantService: TenantService,
-  ) {}
+  constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
   create(@Body() createTransactionDto: CreateTransactionDto) {
@@ -19,7 +15,7 @@ export class TransactionsController {
   }
 
   @Get()
-  findAll(@Req() req) {
+  findAll() {
     return this.transactionsService.findAll();
   }
 }
